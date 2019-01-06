a5db2bf3da4b9393fabe4f989e1333027abd8114
witcher470 <46425124+witcher470@users.noreply.github.com>
2019-01-07 01:41:37 +0600

    Add files via upload

---
+++ Задание 1/HelloWorldApp/HelloWorldApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorldApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {       
+            Console.WriteLine("Hello, world!"); //Вывод текста на консоль
+            Console.WriteLine("Happy New Year! All the best for you!");
+            Console.ReadKey();
+
+        }
+    }
+}
