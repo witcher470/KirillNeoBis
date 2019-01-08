e1842d85ff6a46ce9b1dbf8170a075ef83e53711
witcher470 <46425124+witcher470@users.noreply.github.com>
2019-01-09 05:01:10 +0600

    Add files via upload

---
+++ Zadanie2_3/Zadanie2_3/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Zadanie2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A: // точка возврата
+            Console.Write("Выберите номер товара ");
+            int kod = Convert.ToInt32(Console.ReadLine());
+            int CountOfProducts;
+            Double TotalPrice;
+            
+            switch(kod)
+            {
+                case 0:
+                    break;
+
+                case 1:
+                    Console.WriteLine("Вы выбрали товар № 1\nВыберите количество товара ");
+                    CountOfProducts = Convert.ToInt32(Console.ReadLine());
+                    TotalPrice = 4 * CountOfProducts;
+                    Console.WriteLine($"Total: R$ {TotalPrice} ");
+                    Console.WriteLine("Для выхода из программы нажмите 0");
+                    goto A;
+
+                case 2:
+                    Console.WriteLine("Вы выбрали товар № 2\nВыберите количество товара");
+                    CountOfProducts = Convert.ToInt32(Console.ReadLine());
+                    TotalPrice= 4.50 * CountOfProducts;
+                    Console.WriteLine($"Total: R$ {TotalPrice}");
+                    Console.WriteLine("Для выхода из программы нажмите 0");
+                    goto A;
+
+                case 3:
+                    Console.WriteLine("Вы выбрали товар № 3\nВыберите количество товара");
+                    CountOfProducts = Convert.ToInt32(Console.ReadLine());
+                    TotalPrice = 5 * CountOfProducts;
+                    Console.WriteLine($"Total: R$ {TotalPrice}");
+                    Console.WriteLine("Для выхода из программы нажмите 0");
+                    goto A;
+
+                case 4:
+                    Console.WriteLine("Вы выбрали товар № 4\nВыберите количество товара");
+                    CountOfProducts = Convert.ToInt32(Console.ReadLine());
+                    TotalPrice = 2 * CountOfProducts;
+                    Console.WriteLine($"Total: R$ {TotalPrice}");
+                    Console.WriteLine("Для выхода из программы нажмите 0");
+                    goto A;
+
+                case 5:
+                    Console.WriteLine("Вы выбрали товар № 5\nВыберите количество товара");
+                    CountOfProducts = Convert.ToInt32(Console.ReadLine());
+                    TotalPrice = 1.50 * CountOfProducts;
+                    Console.WriteLine($"Total: R$ {TotalPrice}");
+                    Console.WriteLine("Для выхода из программы нажмите 0");
+                    goto A;
+
+                default:
+                    Console.WriteLine("Такого товара в списке нет");
+                    Console.WriteLine("Для выхода из программы нажмите 0");
+                    goto A;
+            }
+        }
+    }
+}
