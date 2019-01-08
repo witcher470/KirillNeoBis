1f8818d939eb13748541f21048ad1aa6c998108f
witcher470 <46425124+witcher470@users.noreply.github.com>
2019-01-09 05:00:29 +0600

    Add files via upload

---
+++ Zadanie_2.2/Zadanie_2.2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace Zadanie_2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите а ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите b ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите c ");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            if (a > b && a > c) // нахождение наибольшего числа
+            {
+                Console.WriteLine($"{a} eh o maior");
+            }
+            else if (b>a &&b>c)
+            {
+                Console.WriteLine($"{b} eh o maior");
+            }
+            else
+            {
+                Console.WriteLine($"{c} eh o maior");
+            }
+
+            double MaiorAB = (a + b + Math.Abs(a - b)) / 2; // вычисление наибольшего числа по формуле
+            Console.WriteLine($"MaiorAB {MaiorAB} ");
+            Console.ReadKey();
+        }
+    }
+}
