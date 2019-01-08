c3c49d524717cb1536241e83b04d49fc23216082
witcher470 <46425124+witcher470@users.noreply.github.com>
2019-01-09 04:59:22 +0600

    Add files via upload

---
+++ Zadanie_2.1/Zadanie_2/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задание 2.1
+using System;
+
+namespace Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество рабчих "); 
+            int CountOfWorkers = Convert.ToInt32(Console.ReadLine()); //Ввод переменной CountOfWorkers
+
+            Console.Write("Введите количество рабочих часов в 1 месяце ");   
+            int CountOfHours = Convert.ToInt32(Console.ReadLine());   //Ввод переменной CountOfHours
+
+            Console.Write("Введите зарплату за час ");  
+            double DollarsPerHour = Convert.ToDouble(Console.ReadLine()); //Ввод переменной DollarsPerHour
+
+            double Salary = CountOfHours * DollarsPerHour;   //Вычисление Salary
+
+            Console.WriteLine($"NUMBER = {CountOfWorkers}"); //Вывод количества рабочих в консоли
+            Console.WriteLine($"SALARY = U$ {Salary}"); //Вывод зарплаты рабочего за месяц
+            Console.ReadKey();
+
+        }
+    }
+}
