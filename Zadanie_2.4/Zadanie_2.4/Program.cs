3beb84b5913250bbe6d7823d715f1c320a8f7548
witcher470 <46425124+witcher470@users.noreply.github.com>
2019-01-10 01:24:05 +0600

    Add files via upload

---
+++ Zadanie_2.4/Zadanie_2.4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Zadanie_2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A: // точка возврата
+            int x;
+            Console.Write("Введите целое число Х ");
+            x = Convert.ToInt32(Console.ReadLine());
+            if (x >= 1 && x <= 1000) // Проверка,находится ли число в диапазоне от 1 до 1000
+            {
+                for (int i = 0; i <= x; i++)
+                {
+                    if (i % 2 == 1) // нахождение нечётных чисел
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Введите число в диапазоне от 1 до 1000");
+                goto A;
+            }
+        }
+    }
+}
