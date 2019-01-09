43c3c0fca2b516c6e62aff6de8797e2392734c2e
witcher470 <46425124+witcher470@users.noreply.github.com>
2019-01-10 01:24:59 +0600

    Add files via upload

---
+++ Zadanie_2.5/Zadanie_2.5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_2._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+            Random rand = new Random(); //объявление экземпляра класса генерации рандомных чисел
+            for (int i = 0; i < array.Length; i++) //заполнение массива
+            {
+                array[i] = rand.Next(-100, 100);
+                Console.WriteLine(array[i]);
+                if (array[i] <= 0)
+                {
+                    array[i] = 1;
+                }
+            }
+            Console.WriteLine("Изменённый массив:");
+                for (int i = 0; i < array.Length; i++) // замена отрицательных и нулевых элементов массива
+            {
+                Console.WriteLine(array[i]);
+                if (array[i] <= 0)
+                {
+                    array[i] = 1;
+                }               
+            }
+            Console.ReadKey();
+        }
+    }
+}
