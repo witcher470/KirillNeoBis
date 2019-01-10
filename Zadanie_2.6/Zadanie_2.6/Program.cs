9d378a6aa6450fdc70e462f7586b0ea2dd6d1d45
witcher470 <46425124+witcher470@users.noreply.github.com>
2019-01-11 02:27:52 +0600

    Add files via upload

---
+++ Zadanie_2.6/Zadanie_2.6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace SortApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A:
+            Console.Write("Введите порядковый номер числа Фибоначи ");
+            int m = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Fib ({m}) = {Fibonachi(m)}");
+            Console.ReadKey();
+            Console.Clear();
+            goto A;
+             int Fibonachi(int n)
+            {                
+                if (n == 0)
+                {
+                    return 0;
+                }
+                if (n == 1)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return Fibonachi(n - 1) + Fibonachi(n - 2);
+                }
+            }
+        }
+    }
+}
