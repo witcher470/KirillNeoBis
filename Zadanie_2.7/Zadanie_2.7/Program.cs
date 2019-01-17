8ec48c88b0db10a29dbbaf22b60a99b9bd8d3f1a
witcher470 <46425124+witcher470@users.noreply.github.com>
2019-01-17 21:57:55 +0600

    Add files via upload

---
+++ Zadanie_2.7/Zadanie_2.7/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Zadanie_2._7
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+        A:
+            Console.Write("Введите порядок матрицы ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n == 0)
+            {
+                Console.WriteLine("Матрица не может иметь порядок 0");
+                goto A;
+            }
+            else
+                for (int i = 1; i <= n; i++)
+                {
+                    for (int j = 1; j <= n; j++)
+                    {
+                       int  x = i;
+                        if (j < x) x = j;
+                        if (n - i + 1 < x) x = n - i + 1;
+                        if (n - j + 1 < x) x = n - j + 1;
+                        Console.Write(x);
+                        if (j < n) Console.Write(' ');
+                        else Console.Write('\n');
+                    }
+                }
+            Console.Write('\n');
+            goto A;
+
+        } 
+    }
+
+}
