b41fc21d2a99bdc46faa97782eb0369d2258f92b
witcher470 <46425124+witcher470@users.noreply.github.com>
2019-02-17 20:36:41 +0600

    Add files via upload

---
+++ Zadanie_3 (B)/Zadanie_3 (B)/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_3__B_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the string ");
+            string str = Console.ReadLine();
+            Console.WriteLine(extractCurrencyValue(str));
+            Console.ReadKey();
+        }
+        static string extractCurrencyValue(string a)
+        {
+            if (a[0] == '$')
+            {
+                a = a.Trim('$');
+                return a;
+            }
+            else
+            {
+                Console.WriteLine("wrong format");
+
+            }
+            return " ";
+            
+        }
+    }
+}
