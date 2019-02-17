f1e3452bae2744f577071964e4317c309bf2da7d
witcher470 <46425124+witcher470@users.noreply.github.com>
2019-02-17 20:18:55 +0600

    Add files via upload

---
+++ Zadanie_3 (A)/Zadanie_3 (A)/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_3__A_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            int maxlength = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(truncate(str, maxlength));
+
+            Console.ReadKey();
+        }
+        static string truncate(string a, int b)
+        {
+
+            if (a.Length > b)
+            {
+                a = a.Remove(b-3);
+                a = a + "...";
+            }
+            else if (a.Length == b)
+            {
+                a = a.Remove(b - 3, 3);
+                a = a + "...";
+            }
+            else if (a.Length<b)
+            {
+                return a;
+                
+            }
+            return a;
+        }
+    }
+}
